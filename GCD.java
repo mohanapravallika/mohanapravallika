6d6dfdf743f5f7aea1437b06e66bbbdc4426f3fb
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-06-17 20:20:55 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,19 @@
+package Numbers;
+import java.util.*;
+public class GCD {
+	public static void main(String [] args) {
+		try (Scanner obj = new Scanner(System.in)) {
+			System.out.println("Enter 2 numbers: ");
+			int a = obj.nextInt(); // 12
+			int b = obj.nextInt(); // 8
+			int gcd = 1;
+			for (int i = 1; i<=a && i<=b; i++)
+			{
+				if(a%i==0 && b%i==0)
+					gcd=i; // gcd = 4
+			}
+			System.out.println("GCD = " +gcd);
+		}
+}
+}
+
